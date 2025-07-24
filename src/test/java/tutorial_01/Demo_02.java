dac431e0a3ba3d9dba3ebd2d445ba69d474475eb
SSPL <SSPL@SSPL-LAP-166>
2025-07-24 11:00:58 +0530

    Add User Test cases by Sonali automation tester

---
+++ src/test/java/tutorial_01/Demo_02.java
@@ -0,0 +1,11 @@
+package tutorial_01;
+
+public class Demo_02 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Add new user test cases written by Sonali Automation Tester.");
+
+	}
+
+}
