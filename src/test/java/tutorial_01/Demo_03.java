facf88d6d5b32f3a802c1d5d70fdb0aba95341af
SSPL <SSPL@SSPL-LAP-166>
2025-07-24 11:05:40 +0530

    Search Test Cases written by Amit Automation Tester

---
+++ src/test/java/tutorial_01/Demo_03.java
@@ -0,0 +1,10 @@
+package tutorial_01;
+
+public class Demo_03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Search user in PIM page test cases written by Amit Automation Tester.");
+	}
+
+}
